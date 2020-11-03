03bd6718a86b62f78643378520ef67f8c6ffab11
BinnyJ <binnyjohn39@gmail.com>
2020-11-03 14:51:20 -0500

    Test Case one

---
+++ src/muleprojectfolder/paymentone.java
@@ -0,0 +1,12 @@
+package muleprojectfolder;
+
+public class paymentone {
+    public static void main(String[] args) {
+
+        int numOne = 25;
+        int numTwo = 30;
+        int sum = numOne + numTwo;
+        System.out.println("The toltal number is : " + sum);
+
+    }
+}
